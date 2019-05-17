c4278f9442684d9d420efc8bc4d107b69afc39d2
Aryanaut <aryan.mahesh2010@gmail.com>
2019-05-17 20:13:29 +0530

    updated working code

--- code/tote-ultra/tote-ultra.ino
+++ code/tote-ultra/tote-ultra.ino
@@ -11,13 +11,19 @@
 // 1 -- Walk
 // 2 -- Nothing
 int robot_mode = 1;
+const int trigPin = A4;
+const int echo = A5;
 
-
+long duration;
+int distance;
 void setup() {
     //battery_setup();
     servo_setup();
     //beep_setup();
     //ir_setup();
+    pinMode(trigPin, OUTPUT);
+    pinMode(echo, INPUT);
+    Serial.begin(9600);
 }
 
 void loop() {
@@ -32,7 +38,20 @@ void loop() {
             }
             break;
         case 1:
-            walk();
+            digitalWrite(trigPin, LOW);
+            delayMicroseconds(2);
+          
+            digitalWrite(trigPin, HIGH);
+            delayMicroseconds(10);
+            digitalWrite(trigPin, LOW);
+          
+            duration = pulseIn(echo, HIGH);
+            
+            distance = duration*0.034/2;
+            Serial.println(distance);
+            if (distance > 10) {
+              walk();      
+            }
             //walk();
             break;
         case 2: // Nothing
